17bc4f211132f646023a3ee92598351cb868ce4c
tommy-lean <k-tomilin99@mail.ru>
2023-10-02 22:37:27 +0500

    Change

--- front/src/App.tsx
+++ front/src/App.tsx
@@ -13,7 +13,7 @@ function App() {
           <div className="backgroundImage container-fluid">
                 <NaviBar></NaviBar>
                 <AboutUs></AboutUs>
-                <ModalWindow>фывфыв</ModalWindow>
+                <ModalWindow></ModalWindow>
           </div>
       </>
 );
